9bdd5f5bd0ef90071ddc61d77c2ffc133f887e1f
rozena <rrrozyislam@gmail.com>
2017-08-08 14:06:01 +0600

    class-75

---
+++ 100+section/class-75/assets/js/active.js
@@ -0,0 +1,44 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+        
+        $('#bar1').barfiller({
+            barColor: '#ffffff',
+            duration: 2000
+        });
+        $('#bar2').barfiller({
+            barColor: '#ffffff',
+            duration: 2000
+        });
+    	$('#bar3').barfiller({
+            barColor: '#ffffff',
+            duration: 2000
+        });
+        $('#bar4').barfiller({
+            barColor: '#ffffff',
+            duration: 2000
+        });
+
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
