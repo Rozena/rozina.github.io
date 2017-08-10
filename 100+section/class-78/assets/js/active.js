8d583cdde64118acfbe34669c4b4e23f4d3e1ad0
rozena <rrrozyislam@gmail.com>
2017-08-10 12:21:29 +0600

    class78

---
+++ 100+section/class-78/assets/js/active.js
@@ -0,0 +1,30 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+     
+        
+      
+        
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
