f8198fc07a55bebba518620d5c9b0a491428ff41
rozena <rrrozyislam@gmail.com>
2017-08-13 23:23:06 +0600

    class-80

---
+++ 100+section/class-80/assets/js/active.js
@@ -0,0 +1,32 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+     $(".mailchimp-form").ajaxChimp({
+     	url: 'http://netlify.us16.list-manage.com/subscribe/post?u=c2469184e53e2b2f2c0ec5601&amp;id=6d85e55c85'
+     });
+        
+      
+        
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
