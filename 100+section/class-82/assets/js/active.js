db3a59932e262c4e3dc173ae129ea2cd2b006cba
rozena <rrrozyislam@gmail.com>
2017-08-15 15:07:45 +0600

    class-82

--- 100+section/class-82/assets/js/active.js
+++ 100+section/class-82/assets/js/active.js
@@ -2,7 +2,7 @@
     "use strict";
 
     jQuery(document).ready(function($) {
-        
+    
      $(".feature-project").owlCarousel({
      	   items: 1,    
            dots: true,
@@ -14,6 +14,25 @@
 
      	}); 
         
+		$(".project-bg").ripples({
+			resolution: 512,
+			dropRadius: 20,
+			perturbance: 0.04,
+		});  
+
+		// Automatic drops
+		setInterval(function() {
+			var $el = $(".project-bg");
+			var x = Math.random() * $el.outerWidth();
+			var y = Math.random() * $el.outerHeight();
+			var dropRadius = 20;
+			var strength = 0.04 + Math.random() * 0.04;
+			$el.ripples('drop', x, y, dropRadius, strength);
+		}, 400);
+
+
+
+
     });
     
     jQuery(window).load(function(){
