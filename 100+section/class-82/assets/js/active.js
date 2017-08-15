cfc7be9619c628b72f6823dd2e9f99ec494c7340
rozena <rrrozyislam@gmail.com>
2017-08-15 14:21:57 +0600

    class-82

---
+++ 100+section/class-82/assets/js/active.js
@@ -0,0 +1,38 @@
+(function($) {
+    "use strict";
+
+    jQuery(document).ready(function($) {
+        
+     $(".feature-project").owlCarousel({
+     	   items: 1,    
+           dots: true,
+           autoplay: true,
+           loop: true,
+           nav: false,
+           animateOut:"fadeOut",
+           animateIn:"fadeIn"
+
+     	}); 
+        
+    });
+    
+    jQuery(window).load(function(){
+     
+        
+        
+        
+    }); 
+
+}(jQuery));
+
+
+
+
+
+
+
+
+
+
+
+
